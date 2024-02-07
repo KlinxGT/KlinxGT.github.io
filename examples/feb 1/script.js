01cd7cfedbfd43fb25e260b8a283dda3cf88b33b
Xin Lin <xl26@email.sc.edu>
2024-02-06 21:02:41 -0500

    fixes

---
+++ examples/feb 1/script.js
@@ -0,0 +1,32 @@
+let pos = 0;
+const colorSquare =() =>{
+    document.getElementById("square").classList.add("rainbow");
+    //document.querySelector("#square")
+};
+
+const moveDown = () => {
+    const root = document.querySelector(":root");
+    root.style.setProperty("--square-top", "200px")
+};
+
+const addCircle =()=> {
+
+    const color = document.getElementById("txt-color").Value;
+    const root = document.querySelector(":root");
+    root.style.setProperty("--ball-color",color);
+
+
+    const playground = document.getElementById("circle-playground");
+    //playground.innerHTML += <section class = 'circle'></section>;
+    const ball = document.createElement("section");
+    ball.classList.add("circle");
+    playground.append(ball);
+    ball.classList.add("Circle");
+    ball.style.background = color;
+};
+
+
+
+document.getElementById("btn-color").onclick =colorSquare;
+document.getElementById("btn-move-down").onclick = moveDown;
+document.getElementById("btn-add-circle").onclick = addCircle;
