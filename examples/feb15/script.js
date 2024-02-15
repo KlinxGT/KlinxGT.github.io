77f270be53157861951d6416defbf14a37907e85
Xin Lin <xl26@email.sc.edu>
2024-02-15 15:57:25 -0500

    "project"

--- examples/feb15/script.js
+++ examples/feb15/script.js
@@ -1,4 +1,33 @@
 const numLoop =() =>{
+
+    
+    const errorP=document.getElementById("error-num");
+    errorP.innerHTML="";
+
+    //get starting and ending numbers
+    let startNum = document.getElementById("txt-start-num").value;
+    let endNum = document.getElementById("txt-end-num").value;
+
+    console.log(`Looping from ${startNum} to ${endNum}.`)
+
+    
+
+    if(NaN(startNum)|| NaN(endNum) || startNum.trim()=="" || endNum.Num.trim()==""){
+        errorP.innerHTML = "You must enter number(s).";
+        return;
+    }
+
+
+
+    startNum = parseInt(startNum);
+    endNum = parseInt(endNum);
+
+
+    if(startNum >= endNum){
+        errorP.innerHTML ="ending numbere must be larger than starting number";
+        return;
+    } 
+    //Creates an li for each number
     const numList = document.getElementById("num-list");
     for(let i=0; i<10; i++){
         const li = document.createElement("li");
@@ -7,4 +36,9 @@ const numLoop =() =>{
     }
 }
 
+
+const startStopCount =(e) =>{
+    if(e.target.innerHTML.toLowerCase() == "start")
+    
+}
 document.getElementById("btn-loop-nums").onclick = numLoop;
