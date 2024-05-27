0da6b3669510aeac23a65681418ee86942371304
Xin Lin <xl26@email.sc.edu>
2024-05-27 11:13:04 -0400

    Fixes

--- CSCE242Summer/Projects/Part5/HBS/script.js
+++ CSCE242Summer/Projects/Part5/HBS/script.js
@@ -2,8 +2,6 @@ document.getElementById("hamburger").onclick = () => {
     document.querySelector("#nav-bar ol").classList.toggle("hidden");
 }
 
-
-
 const getPost = async() => {
     try {
         return((await fetch("https://klinxgt.github.io/CSCE242Summer/Projects/Part5/HBS/jason.json")).json());
@@ -13,21 +11,64 @@ const getPost = async() => {
     }
 }
 
+const getColum = () => {
+    const col1 = document.getElementById("col1");
+    const col2 = document.getElementById("col2");
+    const col3 = document.getElementById("col3");
+    const col0 = document.getElementById("col0");
+
+    let theOne = col1;
+    
+    if(theOne.offsetHeight > col2.offsetHeight) {
+        theOne = col2;
+    }
+    if(theOne.offsetHeight > col3.offsetHeight) {
+        theOne = col3;
+    }
+    if(theOne.offsetHeight > col0.offsetHeight) {
+        theOne = col0;
+    }
+
+    return theOne;
+}   
+
 const postPage = async() => {
     posts = await getPost();
     posts.forEach(async (post) => {
-        sec = document.createElement("section");
+        const sec = document.createElement("section");
         sec.classList.add("post");
 
-        img = document.createElement("img");
+        const img = document.createElement("img");
         img.src = post.image;
-        img.style.width = "100";
+        img.classList.add("postImg");
         sec.append(img);
         
-        cap = document.createElement("p");
-        cap.innerText = post.caption;
+        const cap = document.createElement("p");
+        cap.textContent = post.caption;
+        sec.append(cap);
 
-        document.querySelector(`#col${post._id%4}`).append(sec);
+        const likeSec = document.createElement("section");
+        likeSec.classList.add("like-count");
+
+        const likes = document.createElement("button");
+        likes.classList.add("likes");
+        likes.innerText = "Like";
+        likeSec.append(likes);
+
+        const count = document.createElement("h2");
+        count.textContent = "0";
+        likeSec.append(count);
+
+        const dislikes = document.createElement("button");
+        dislikes.classList.add("dislikes");
+        dislikes.innerHTML = "Dislike";
+        likeSec.append(dislikes);
+
+        sec.append(likeSec);
+        
+
+        const col = getColum();
+        col.append(sec);
     });
 }
 
