13b197b030564372b2cfa16f4b0f050decca3548
Xin Lin <xl26@email.sc.edu>
2024-05-27 00:14:42 -0400

    hey

--- CSCE242Summer/Projects/Part5/HBS/script.js
+++ CSCE242Summer/Projects/Part5/HBS/script.js
@@ -2,13 +2,34 @@ document.getElementById("hamburger").onclick = () => {
     document.querySelector("#nav-bar ol").classList.toggle("hidden");
 }
 
+
+
 const getPost = async() => {
     try {
-        console.log(fetch(jason.json()));
+        return((await fetch("https://klinxgt.github.io/CSCE242Summer/Projects/Part5/HBS/jason.json")).json());
     }
     catch (error) {
         console.log(error);
     }
 }
 
-window.onload = getPost;
+const postPage = async() => {
+    posts = await getPost();
+    posts.forEach(async (post) => {
+        sec = document.createElement("section");
+        sec.classList.add("post");
+
+        img = document.createElement("img");
+        img.src = post.image;
+        img.style.width = "100";
+        sec.append(img);
+        
+        cap = document.createElement("p");
+        cap.innerText = post.caption;
+
+        document.querySelector(`#col${post._id%4}`).append(sec);
+    });
+}
+
+
+window.onload = postPage;
