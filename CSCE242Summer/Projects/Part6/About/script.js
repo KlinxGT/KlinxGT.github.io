6cd05a27ce744f04439cf4af2cad9199284e880e
Xin Lin <xl26@email.sc.edu>
2024-05-28 16:47:57 -0400

    fixes

--- CSCE242Summer/Projects/Part6/About/script.js
+++ CSCE242Summer/Projects/Part6/About/script.js
@@ -1,3 +1,4 @@
 document.getElementById("hamburger").onclick = () => {
     document.querySelector("#nav-bar ol").classList.toggle("hidden");
 }
+
--- CSCE242Summer/Projects/Part6/Post/script.js
+++ CSCE242Summer/Projects/Part6/Post/script.js
@@ -2,13 +2,42 @@ document.getElementById("hamburger").onclick = () => {
     document.querySelector("#nav-bar ol").classList.toggle("hidden");
 }
 
-const getHome = async () => {
-    try {
-        const home = await fetch("../HBS/index.html");
-        console.log(home);
-    } catch (error) {
-        console.log(error);
-    }
+const content = document.getElementById("content");
+const ul = document.createElement("ul");
+let count = 1;
+ul.id = "list";
+
+
+content.append(ul);
+
+const list = (e) => {
+    e.preventDefault();
+    const sec = document.createElement("section");
+    sec.classList.add("post");
+
+    const counter = document.createElement("h5");
+    counter.style.textAlign = "center";
+    counter.innerHTML = `Post: ${count++}`;
+    sec.append(counter);
+
+    const pic = document.createElement("p");
+    pic.textContent = `Image File: ${(document.getElementById("pic").value)}`;
+    sec.append(pic);
+
+    const cap = document.createElement("p");
+    cap.innerText = `Captions: ${document.getElementById("caption").value}`;
+    sec.append(cap);
+    
+    const des = document.createElement("p");
+    des.innerText = `Extra info: ${document.getElementById("description").value}`;
+    sec.append(des);
+    
+    const tags = document.createElement("p");
+    tags.innerText = `Tags: ${document.getElementById("tags").value}`;
+    sec.append(tags);
+
+    ul.append(sec);
 }
 
-getHome();
+form.onsubmit = list;
+
